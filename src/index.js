b980e3babb4db2f5f4330ac36bacbb6ecacffc21
Dan Lubbers <lubbers.dan@gmail.com>
2019-12-17 23:18:39 -0500

    Starting to refactor code I am mapping over and displaying in the DOM

--- src/index.js
+++ src/index.js
@@ -54,46 +54,52 @@ const searchUserLocation = () => {
           const region = document.querySelector('#region');
           region.textContent = `${userLocation.data.region}`;
 
-          const trails = await axios.get(`${cors}https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${long}&maxDistance=10&key=${apiTrailsKey}`);
-          console.log(trails.data.trails[0].name);
+          // const trails = await axios.get(`${cors}https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${long}&maxDistance=10&key=${apiTrailsKey}`);
+          // console.log(trails.data.trails[0].name);
 
           // STATIC TESTING for Boulder, CO
-          // const trails = await axios.get(`${cors}https://www.hikingproject.com/data/get-trails?lat=40.0150&lon=-105.2705&maxDistance=10&key=${apiTrailsKey}`);
+          const trails = await axios.get(`${cors}https://www.hikingproject.com/data/get-trails?lat=40.0150&lon=-105.2705&maxDistance=10&key=${apiTrailsKey}`);
 
           // const name = document.querySelector('#name');
           // name.textContent = trails.data.trails[0].name;
 
-        let list = document.createElement('ul');
-        list.className = 'trail-list';
+        // let list = document.createElement('ul');
+        // list.className = 'trail-list';
 // *** Must use at least 1 higher order function *** //
-        trails.data.trails.forEach((e, i) => {
-          let p = document.createElement('p');
-          let img = document.createElement('img');
-          // let aTag = document.createElement('a');
-          let div = document.createElement('div');
-          p.className = 'trail-info';
-          img.className = 'trail-image';
-          // aTag.className = 'url';
-          div.className = 'horizontal-line'
-          p.textContent = `${e.name} \nLocation: ${e.location} \nDescription: ${e.summary}`; 
-          // aTag.setAttribute('href', e.url);
-          // aTag.innerText = `URL: ${e.name}`;
-          img.src = `${e.imgSmallMed}`     
-          
-          list.appendChild(p)
-          // list.appendChild(aTag)
-// *** Must display images *** //          
-          list.appendChild(img)
-          list.appendChild(div)
-            
+        let trailInfo = trails.data.trails.map((e, i) => {
+          return `
+              <div key='${++i}' style=' margin: 20px 0;
+                                        padding: 0;
+                                        display: flex;
+                                        flex-direction: column;
+              '>
+                <h2 style='margin: 5px 0; padding-left: 5px;'>${e.name}</h2>
+                <h3 style='margin: 5px 0; padding-left: 5px;'>${e.location}</h3>
+                <p style='padding-left: 5px;' class="">${e.summary}</p>
+                <div style='display: flex; justify-content: center;'>
+                <img style='width: 400px; height: 100%;' src='${e.imgSmallMed}' alt='${e.name}' />
+                </div>
+                <div style='  margin: 20px 0;
+                              display: block;
+                              overflow: hidden;
+                              border-style: solid;
+                              border-width: 1px;
+                              color: rgb(150, 150, 150);
+                '>
+                </div>
+              </div>
+          `;
+        }).join('');
 
-          // let linkList = document.querySelectorAll('a');
-          // console.log(linkList);
-          // linkList.forEach(e => e.setAttribute('target', '_blank'));
-          });
-          
-        let trailInfoContainer = document.querySelector('#trail-info');
-        trailInfoContainer.appendChild(list)
+        // console.log(Array.isArray(trailInfo));
+        // console.log(typeof trailInfo);
+        
+        // turn a string into a DOM element
+        const trailInfoFragment = document.createRange().createContextualFragment(trailInfo)
+        console.log(typeof trailInfoFragment);
+        
+        const trailContainer = document.querySelector('#trail-container')
+        trailContainer.appendChild(trailInfoFragment);
         
       } catch (err) {
         return console.error(err)
@@ -107,77 +113,77 @@ const apiLocator = document.querySelector('#apiLocator');
 apiLocator.addEventListener("click", searchUserLocation);
 
 
-const searchUserInput = () => {
+// const searchUserInput = () => {
 
-  if(!cityInput && !stateInput) alert('You can not search without first adding a city and state');
+//   if(!cityInput && !stateInput) alert('You can not search without first adding a city and state');
 
 
   
-  ( async () => {
+//   ( async () => {
     
-    try {
+//     try {
 
-      // Using the global variables cityInput and stateInput, I inject those values into the api to get the region the user wants to search
-      const userInput = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${cityInput}%20${stateInput}&key=${apiOpenCageKey}`);
+//       // Using the global variables cityInput and stateInput, I inject those values into the api to get the region the user wants to search
+//       const userInput = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${cityInput}%20${stateInput}&key=${apiOpenCageKey}`);
       
-      // console.log(userInput.data.results[0].geometry);
+//       // console.log(userInput.data.results[0].geometry);
 
-      const lat = userInput.data.results[0].geometry.lat;
-      console.log('lat', lat);
-      const long = userInput.data.results[0].geometry.lng;
-      console.log('long', long);
+//       const lat = userInput.data.results[0].geometry.lat;
+//       console.log('lat', lat);
+//       const long = userInput.data.results[0].geometry.lng;
+//       console.log('long', long);
       
-      const areaContainer = document.querySelector('#area-container');
-      areaContainer.style.display = "flex";
+//       const areaContainer = document.querySelector('#area-container');
+//       areaContainer.style.display = "flex";
 
-      const city = document.querySelector('#city');
-      // Capitalizes every first letter
-      let cityInputCaps = cityInput.split(' ').map(word => {
-        return word.slice(0, 1).toUpperCase() + word.slice(1);
-      }).join(' ');
+//       const city = document.querySelector('#city');
+//       // Capitalizes every first letter
+//       let cityInputCaps = cityInput.split(' ').map(word => {
+//         return word.slice(0, 1).toUpperCase() + word.slice(1);
+//       }).join(' ');
 
-      city.textContent = `${cityInputCaps}`;
-      const region = document.querySelector('#region');
-      region.textContent = `${stateInput.toUpperCase()}`;
+//       city.textContent = `${cityInputCaps}`;
+//       const region = document.querySelector('#region');
+//       region.textContent = `${stateInput.toUpperCase()}`;
 
-      const trails = await axios.get(`${cors}https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${long}&maxDistance=10&key=${apiTrailsKey}`);
-      console.log(trails.data);
+//       const trails = await axios.get(`${cors}https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${long}&maxDistance=10&key=${apiTrailsKey}`);
+//       console.log(trails.data);
 
 
-      let list = document.createElement('ul');
-      list.className = 'trail-list';
+//       let list = document.createElement('ul');
+//       list.className = 'trail-list';
 
-      trails.data.trails.forEach((e, i) => {
-        let p = document.createElement('p');
-        let img = document.createElement('img');
+//       trails.data.trails.forEach((e, i) => {
+//         let p = document.createElement('p');
+//         let img = document.createElement('img');
         
-        let div = document.createElement('div');
-        p.className = 'trail-info';
-        img.className = 'trail-image';
+//         let div = document.createElement('div');
+//         p.className = 'trail-info';
+//         img.className = 'trail-image';
         
-        div.className = 'horizontal-line'
-        p.textContent = `${e.name} \nLocation: ${e.location} \nDescription: ${e.summary}`; 
+//         div.className = 'horizontal-line'
+//         p.textContent = `${e.name} \nLocation: ${e.location} \nDescription: ${e.summary}`; 
         
-        img.src = `${e.imgSmallMed}`     
+//         img.src = `${e.imgSmallMed}`     
         
-        list.appendChild(p)
+//         list.appendChild(p)
         
-        list.appendChild(img)
-        list.appendChild(div)
+//         list.appendChild(img)
+//         list.appendChild(div)
           
-        });
+//         });
         
-      let trailInfoContainer = document.querySelector('#trail-info');
-      trailInfoContainer.appendChild(list)
+//       let trailInfoContainer = document.querySelector('#trail-info');
+//       trailInfoContainer.appendChild(list)
       
-      // const locationResultsContainer = document.querySelector('#location-results-container');
-      // locationResultsContainer.style.display = "none";
+//       // const locationResultsContainer = document.querySelector('#location-results-container');
+//       // locationResultsContainer.style.display = "none";
 
-    } catch (err) {
-      return console.error(err)
-    }
-  }) ();
-};
+//     } catch (err) {
+//       return console.error(err)
+//     }
+//   }) ();
+// };
 
-const userSearchInput = document.querySelector('#userSearchInput');
-userSearchInput.addEventListener("click", searchUserInput);
+// const userSearchInput = document.querySelector('#userSearchInput');
+// userSearchInput.addEventListener("click", searchUserInput);
